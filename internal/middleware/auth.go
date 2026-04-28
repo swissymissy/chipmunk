0a2d02715b0e5d581ccf84c35fb986ac1cacdb50
swissymissy <pnghi1606@gmail.com>
2026-04-27 22:16:08 -0400

    add middlewere AuthRequired

---
+++ internal/middleware/auth.go
@@ -0,0 +1,32 @@
+package middleware
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/swissymissy/chipmunk/internal/auth"
+)
+
+func AuthRrequired(next http.HandlerFunc, jwtSecret string ) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// check for token bearer header
+		token, err := auth.GetBearerToken(r.Header)
+		if err != nil {
+			log.Printf("No bearer token: %s\n", err)
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return
+		}
+
+		// validate token
+		studentID, err := auth.ValidateJWT(token, jwtSecret)
+		if err != nil {
+			log.Printf("invalid token: %s\n", err)
+			http.Error(w, "Unauthorized", http.StatusUnauthorized)
+			return 
+		}
+
+		// valid token, let request go through
+		log.Printf("student with ID: %s , has logged in\n", studentID)
+		next.ServeHTTP(w, r)
+	})
+}
