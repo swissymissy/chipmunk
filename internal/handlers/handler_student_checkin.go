acc3463a5d1a6b25a773fc4076d8c3c284467d76
swissymissy <pnghi1606@gmail.com>
2026-04-30 17:24:36 -0400

    add TODO

---
+++ internal/handlers/handler_student_checkin.go
@@ -0,0 +1,9 @@
+package handlers
+
+import "net/http"
+
+func (cfg *ApiConfig) HandlerStudentCheckIn(w http.ResponseWriter, r *http.Request) {
+	//TODO: phase 3
+
+	ResponseWithError(w, http.StatusNotImplemented, "not implemented yet")
+}
