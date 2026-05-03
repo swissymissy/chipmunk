28cda46022090f07035a6de6e21157cbada6a40f
swissymissy <pnghi1606@gmail.com>
2026-05-02 22:50:36 -0400

    add: reset courses table handler

---
+++ internal/handlers/handler_delete_course.go
@@ -0,0 +1,28 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+)
+
+// reset courses table
+func (cfg *ApiConfig) HandlerResetCourses(w http.ResponseWriter, r *http.Request) {
+	// check
+	if cfg.Platform != "dev" && cfg.Platform != "prof" {
+		ResponseWithError(w, http.StatusUnauthorized, "unauthorized")
+		return
+	}
+
+	err := cfg.DB.DeleteAllCourse(r.Context())
+	if err != nil {
+		log.Printf("error reseting courses table: %s\n", err)
+		ResponseWithError(w, http.StatusInternalServerError, "failed to reset table")
+		return
+	}
+
+	ResponseWithJSON(w, http.StatusOK, struct{
+		Message string `json:"message"`
+	}{
+		Message: "Successfully reset all courses.",
+	})
+}
