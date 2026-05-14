a5b2176d4f7dd34df162e3d24cd7a17b2d30d3d1
swissymissy <pnghi1606@gmail.com>
2026-05-14 10:48:07 -0400

    add: remove a course endpoint and handler

---
+++ internal/handlers/handler_delete_a_course.go
@@ -0,0 +1,31 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+)
+
+// let professor remove a course from the list
+func (cfg *ApiConfig) HandlerRemoveCourse(w http.ResponseWriter, r *http.Request) {
+	// get course id from url
+	course_id := r.PathValue("id")
+	if course_id == "" {
+		ResponseWithError(w, http.StatusBadRequest, "course id can't be empty")
+		return
+	}
+
+	// remove a course in the list
+	err := cfg.DB.DeleteCourse(r.Context(), course_id)
+	if err != nil {
+		log.Printf("error removing a course in list: %s\n", err)
+		ResponseWithError(w, http.StatusInternalServerError, "failed to remove course")
+		return
+	}
+
+	ResponseWithJSON(w, http.StatusOK, struct {
+		Msg string `json:"msg"`
+	}{
+		Msg: "Course has been removed.",
+	})
+
+}
