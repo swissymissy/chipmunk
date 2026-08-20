c3c1bb9c538865a8ef3590c6a0c9c8c40efb5ac1
swissymissy <pnghi1606@gmail.com>
2026-08-20 15:24:01 -0400

    fix: typos in handler

--- internal/handlers/handler_add_students_to_past_sessions.go
+++ internal/handlers/handler_add_students_to_past_sessions.go
@@ -12,9 +12,17 @@ import (
 
 // handle the request of adding students to past sessions
 func (cfg *ApiConfig) HandlerAddStudentToPastSessionRecord(w http.ResponseWriter, r *http.Request) {
+	sessionIDStr := r.PathValue("session_id")
+	sessionID, err := strconv.ParseInt(sessionIDStr, 10, 64)
+	if err != nil {
+		log.Printf("error converting session id to int: %s\n", err)
+		ResponseWithError(w, http.StatusBadRequest, "invalid session id")
+		return
+	}
+
 	// decode request
 	var student AddStudentToPastSessionRecordReq
-	err := DecodeRequest(r, &student)
+	err = DecodeRequest(r, &student)
 	if err != nil {
 		log.Printf("error decoding student id: %s\n", err)
 		ResponseWithError(w, http.StatusBadRequest, "invalid student id")
@@ -28,29 +36,21 @@ func (cfg *ApiConfig) HandlerAddStudentToPastSessionRecord(w http.ResponseWriter
 		return
 	}
 
-	sessionIDStr := r.PathValue("session_id")
-	sessionID, err := strconv.ParseInt(sessionIDStr, 10, 64)
-	if err != nil {
-		log.Printf("error converting seesion id to int: %s\n", err)
-		ResponseWithError(w, http.StatusBadRequest, "invalid session id")
-		return
-	}
-
 	// call query to db
 	record, err := cfg.DB.AddStudentToSession(r.Context(), database.AddStudentToSessionParams{ID: sessionID, StudentID: student.StudentID})
 	if err != nil {
 		if errors.Is(err, sql.ErrNoRows) {
 			log.Print("error adding student to session. Student isn't enrolled in this course or already in this session")
-			ResponseWithError(w, http.StatusBadRequest, "unable to add to student to session. Student isn't enrolled in this course or already in this session")
+			ResponseWithError(w, http.StatusBadRequest, "unable to add student to session. Student isn't enrolled in this course or already in this session")
 			return
 		}
 		log.Printf("error adding student to this session: %s\n", err)
-		ResponseWithError(w, http.StatusInternalServerError, "unable to add studen to this session.")
+		ResponseWithError(w, http.StatusInternalServerError, "unable to add student to this session.")
 		return
 	}
 
 	// response
-	ResponseWithJSON(w, 201, AddStudentToPastSessionRecordRes{
+	ResponseWithJSON(w, http.StatusCreated, AddStudentToPastSessionRecordRes{
 		Msg:       "Student has been added to session successfully",
 		SessionID: record.SessionID,
 		StudentID: record.StudentID,
